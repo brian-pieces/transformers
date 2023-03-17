006aab9100db6364b31748192480166eadf6d154
ydshieh <ydshieh@users.noreply.github.com>
2023-03-17 19:06:31 +0100

    update script

--- utils/update_tiny_models.py
+++ utils/update_tiny_models.py
@@ -1,13 +1,23 @@
+import json
+
 from create_dummy_models import create_tiny_models
 
 if __name__ == "__main__":
 
+    with open("tests/utils/tiny_model_summary.json") as fp:
+        tiny_model_info = json.load(fp)
+
+    existing_model_classes = {}
+    for name in tiny_model_info:
+        existing_model_classes.update(tiny_model_info[name]["model_classes"])
+    existing_model_classes = sorted(list(existing_model_classes))
+
     output_path = "tiny_models"
     all = True
     model_types = None
-    models_to_skip = []
+    models_to_skip = existing_model_classes
     no_check = True
-    upload = True
+    upload = False
     organization = "hf-internal-testing"
 
     create_tiny_models(
